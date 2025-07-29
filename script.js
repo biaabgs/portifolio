e0a60a1ae39db5df0164500f7533462ddfe0dad3
BEATRIZ GON�ALVES DA SILVA <25171016@educ123.sp.senai.br>
2025-07-29 16:50:27 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,5 @@
+//header interativo
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('.header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})
