4affc22501b0120230856dbc591490501665df69
BEATRIZ GON�ALVES DA SILVA <25171016@educ123.sp.senai.br>
2025-07-31 16:27:55 -0300

    commit terceiro dia

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ window.addEventListener('scroll', function(){
     var header = document.querySelector('.header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+//scroll parte específica
+function scrollWin() {
+    window.scrollTo({
+        top: 0,  // ajuste da posição vertical
+        left: 0,    // posição horizontal
+        behavior: 'smooth'  // Scroll suave
+    });
+}
+
+function scrollWin2() {
+    window.scrollTo({
+        top: 2800,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollWin3() {
+    window.scrollTo({
+        top: 200,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollWin4() {
+    window.scrollTo({
+        top: 4250,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
